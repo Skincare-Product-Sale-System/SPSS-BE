eee8a8a398c6c8da540afdebe87b7b5644ba0095
Son Nguyen <sonnguyen130504@gmail.com>
2026-01-16 11:41:06 +0700

    update: modify add row version migration

---
+++ BusinessObjects/Migrations/20260116042738_AddRowVersionToProductItem.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessObjects.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRowVersionToProductItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "ProductItems",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedBy",
+                table: "ChatHistories",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "ProductItems");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedBy",
+                table: "ChatHistories",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
